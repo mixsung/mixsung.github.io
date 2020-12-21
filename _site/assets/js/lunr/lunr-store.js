5792e3c7fa1b21545efdd8ee4c71b6ce53724f09
Misung Kim <misung178@gmail.com>
2020-12-21 23:48:48 +0900

    Upload a python-update post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "맥북에서 python 버전 업그레이드 하기",
+        "excerpt":"맥북은 python이 자동으로 깔려있습니다. 2.7버전이 깔려있어 최신 버전 3.9로 업그레이드하기 위해 이것저것 만져보았습니다. Homebrew 설치 맥과 리눅스를 위한 패키지 관리자입니다. 터미널을 열어 아래의 코드를 붙여넣어 실행합니다. /bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" Press RETURN ~ 메세지가 나오면 엔터를 누르고 기다리면 됩니다. brew help help를 입력해서 명령방법이 출력되면 설치 완료! Homebrew 공식...","categories": [],
+        "tags": ["python3","맥북","파이썬","업그레이드"],
+        "url": "http://localhost:4000/python-update/",
+        "teaser": null
+      }]
